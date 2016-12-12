be91caa3d1db6e7992af59274574b4d849174b3b
Besart Aliju <alijubesart@gmail.com>
2016-12-12 16:01:38 -0500

    Final commit before presentation, added templates and some additional logic

--- website/website/urls.py
+++ website/website/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from business import views
+
 
 app_name = 'business'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^register/$', views.UserFormView.as_view(), name='register'),
     url(r'^admin/', admin.site.urls),
+    url(r'^business/', include('business.urls')),
+    url(r'^', include('business.urls')),
 ]
