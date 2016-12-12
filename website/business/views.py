a3c605bea08ea62a8e616872895d481575064aa7
Besart Aliju <alijubesart@gmail.com>
2016-12-12 04:41:01 -0500

    Updated User models

--- website/business/views.py
+++ website/business/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def update_profile(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.profile.bio = 'Lorem ipsum dolor....'
+    user.save()
